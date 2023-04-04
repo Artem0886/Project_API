a5c3ffaff826b7c09baccdc89880feac6316260b
Artem0886 <temagaliev86@gmail.com>
2023-04-05 04:47:00 +0500

    Project_API

---
+++ apiProject/tests/test_google_maps_api.py
@@ -0,0 +1,48 @@
+import json
+import allure
+from utils.api import Google_maps_api
+from utils.checking import Checking
+
+@allure.epic("Test create place")
+class Test_create_place():
+
+    @allure.description("Test create, update, delete new place")
+    def test_create_new_place(self):
+
+        print("Метод POST")
+        result_post = Google_maps_api.create_new_place()
+        check_post = result_post.json()
+        place_id = check_post.get("place_id")
+        Checking.check_status_code(result_post, 200)
+        Checking.check_json_token(result_post, ['status', 'place_id', 'scope', 'reference', 'id'])
+        token = json.loads(result_post.text)
+        print(list(token))
+
+        print("Метод GET POST")
+        result_get = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
+        token = json.loads(result_get.text)
+        print(list(token))
+
+        print("Метод PUT")
+        result_put = Google_maps_api.put_new_place(place_id)
+        Checking.check_status_code(result_put, 200)
+        Checking.check_json_token(result_put, ['msg'])
+
+        print("Метод GET PUT")
+        result_get = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 200)
+        Checking.check_json_token(result_get, ['location', 'accuracy', 'name', 'phone_number', 'address', 'types', 'website', 'language'])
+
+        print("Метод DELETE")
+        result_delete = Google_maps_api.delete_new_place(place_id)
+        Checking.check_status_code(result_delete, 200)
+        Checking.check_json_token(result_delete, ['status'])
+
+        print("Метод GET DELETE")
+        result_get = Google_maps_api.get_new_place(place_id)
+        Checking.check_status_code(result_get, 404)
+        Checking.check_json_token(result_get, ['msg'])
+
+        print("ТЕСТИРОВАНИЕ: СОЗДАНИЯ, ИЗМЕНЕНИЯ И УДАЛЕНИЯ НОВОЙ ЛОКАЦИИ, ПРОШЛО УСПЕШНО")
